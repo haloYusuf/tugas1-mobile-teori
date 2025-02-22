feb3c26429bbdf908df4156239a18820b0530e3b
yusuf <diandrayusuf23@gmail.com>
2025-02-22 14:37:11 +0700

    Update: Create Calculator Page

---
+++ lib/app/pages/kalkulator_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class KalkulatorPage extends StatelessWidget {
+  const KalkulatorPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
